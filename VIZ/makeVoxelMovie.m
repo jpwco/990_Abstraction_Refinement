ef92a1497149ac419813e1334eba763e1db94418
jpwco <github@jpwco.com>
2014-12-07 09:58:35 -0600

    added multiple resolutions and playback speeds

--- VIZ/makeVoxelMovie.m
+++ VIZ/makeVoxelMovie.m
@@ -1,6 +1,21 @@
 %1. IMPORT DATA - ASSUME ITs CALLED 'plotIt'  (CSV of X Y Z points)
 %2. DEPENDS ON PLOTCUBE - http://www.mathworks.com/matlabcentral/fileexchange/15161-plotcube
 
+% 0.10 M version
+% fig = figure('Position', [100, 100, 1049, 895]);
+% for n = 1:length(plotIt)
+%     plotcube([1 1 1], plotIt(n,:), .6, [0 0.5 plotIt(n,3)/16]);
+%     axis([0 50 0 50 0 25]);  % AXIS SIZE
+%     M(n) = getframe
+% end
+
+% 2.0 M version
+% fig = figure('Position', [100, 100, 1049, 895]);
+% for n = 1:length(plotIt)
+%     plotcube([1 1 1], plotIt(n,:), .6, [0 0.5 plotIt(n,3)/5]);
+%     axis([0 2 0 2 0 2]);  % AXIS SIZE
+%     M(n) = getframe;
+% end
 
 fig = figure('Position', [100, 100, 1049, 895]);
 for n = 1:length(plotIt)
@@ -10,7 +25,9 @@ for n = 1:length(plotIt)
 end
 
 
+
+
 % figure
 % movie(M,5,6)  % 5 = number of times, 6 = fps
 
-% movie2avi(M,'myavifile.avi', 'fps', 6)  % SAVE MOVIE TO FILE
+%movie2avi(M,'myavifile2.avi', 'fps', 1)  % SAVE MOVIE TO FILE
